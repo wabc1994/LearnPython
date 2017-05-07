5cd72bb5677785a4fe6e53bafa97b732602eabc7
wabc1994 <liuxiongcheng145@gmail.com>
2017-05-07 17:03:12 +0800

    how to use staticmethod

--- test2/classmethod/testforclassmethod.py
+++ test2/classmethod/testforclassmethod.py
@@ -1,5 +1,6 @@
 #-*-coding:utf-8-*-
 #http://stackoverflow.com/questions/12179271/meaning-of-classmethod-and-staticmethod-for-beginner?noredirect=1&lq=1
+#the url above is a wonderful account for classmethod use in python
 class Date(object):
     def __init__(self,day=0,month=0,year=0):
         self.day=day
@@ -10,6 +11,11 @@ class Date(object):
         day,month,year=map(int,data_as_string.split('-'))
         date1=cls(day,month,year)
         return date1
+    @staticmethod
+    def is_date_valid(date_as_string):
+        day,month,year=map(int ,date_as_string.split('-'))
+        return day<=31 and month <=12 and year<=3999
+is_date=Date.is_date_valid('11-0-2012')
 date2=Date.from_string('11-09-2013')
 print date2.day
 print date2.month
