4011b1becbaf4648795e332987d900e4b42dd3e2
wabc1994 <liuxiongcheng145@gmail.com>
2017-05-12 18:34:52 +0800

    how to use open and read in fileio

---
+++ Fileio/read.py
@@ -0,0 +1,12 @@
+#-*- coding:utf-8 -*-
+
+#将文本作为一个字符串读取
+#with open('somefile.txt','rt') as f:
+ #   data=f.read()
+#读取每一行字符串
+#with open('somefile.txt','rt') as f:
+  #  for line in f:
+
+with open('somefile.txt','rt') as f1:
+    data=f1.read()
+print(data)
