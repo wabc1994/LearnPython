8868978e96da54053b4aeb9466d7c9340844f20d
wabc1994 <liuxiongcheng145@gmail.com>
2017-05-12 18:15:45 +0800

    在csv问价中如何使用字典形式存储

--- Fileio/csvfiledeal.py
+++ Fileio/csvfiledeal.py
@@ -21,3 +21,23 @@ stock=namedtuple('stock',stockset)
 dataset=['2017.2.3','999yuan','yes']
 row=stock(*dataset)
 print row.time
+#access the elements of each row using the row headers for row['Symbol'],  or row['change']
+with open('stock.csv') as f:
+    f_csv=csv.DictReader(f)
+    for row in f_csv:
+        print row['Symbol']
+    #show how to write csv data,
+headers=[('Symbol','price','Date','Time','Change','Volume')]
+rows=[('aa',39.48,'6/11/2007','9:36am',-0.18,181800),('bb',39.48,'6/11/2007','9:36am',-0.18,181800)]
+with open('stocks.csv','w') as f:
+    f_csv=csv.writer(f)
+    f_csv.writerows(headers)
+    f_csv.writerows(rows)
+#如何向文本中写入字典形式
+headers=['Symbol','Price','Date','Time','Change','Volume']
+rows=[{'Symbol':'AA','Price':39.48,'Date':'6/11/2007','Time':'9:36am','Change':-0.18,'Volume':1818000},
+      {'Symbol':'AA','Price':39.48,'Date':'6/11/2007','Time':'9:36am','Change':-0.18,'Volume':1818000},]
+with open('stockdic.cvs','w') as f:
+    f_csv=csv.DictWriter(f,headers)
+    f_csv.writeheader()
+    f_csv.writerows(rows)
