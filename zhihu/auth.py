d6d0e3cbbf6527e7ac8b79ab6fe04dd17f7a34c9
wabc1994 <liuxiongcheng145@gmail.com>
2017-05-18 11:55:10 +0800

    编写验证是否登录成功

---
+++ zhihu/auth.py
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+import requests
+import requests.utils
+
+from zhihu.models.account import Account
+
+try:
+    input = raw_input  # py2
+except:
+    pass
+
+
+def need_login(func):
+    """
+    用户认证装饰器(判断用户是否已经登录)
+    """
+
+    def wrapper(self, *args, **kwargs):
+        success = False
+        if 'z_c0' not in requests.utils.dict_from_cookiejar(self._session.cookies):
+            while not success:
+                account = input("请输入Email或者手机号码:")
+                password = input("请输入密码:")
+                success = Account().login(account, password)
+            if success:
+                self._session.cookies.load(ignore_discard=True)
+
+        return func(self, *args, **kwargs)
+
+    return wrapper
