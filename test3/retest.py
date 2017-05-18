2a2d83a5350fd5ec70e1b52029869cc57e868aeb
wabc1994 <liuxiongcheng145@gmail.com>
2017-05-18 11:13:59 +0800

    re 中的exception 和search match区别

--- test3/retest.py
+++ test3/retest.py
@@ -1,5 +1,8 @@
 #encoding:utf-8
 import re
+class ZhihuError(Exception):
+    def __init__(self,*args,**kwargs):
+        Exception(self,*args,**kwargs)
 def log(email,phone):
     email_regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
     phone_regex = r"(^1\d{10}$)"
@@ -10,3 +13,14 @@ def log(email,phone):
     elif phone_pattern.match(phone):
         print 'phone'
 log('liuxiongchenggmail.com','13456789101')
+
+def _user_slug(profile_url):
+    pattern = re.compile("https?://www.zhihu.com/people/([\w-]+)")
+    match=pattern.search(profile_url)
+    match1=pattern.match(profile_url)
+    if  match:
+        user_slug =match.group(1)
+        return user_slug
+    else:
+        raise ZhihuError("invalid profile url")
+print _user_slug('https://www.zhihu.com/people/sjk')
