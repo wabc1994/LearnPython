03ba4d8d44b49ab17a048e76a0c7a33da23a0e67
wabc1994 <liuxiongcheng145@gmail.com>
2017-05-18 16:45:51 +0800

    测试函数

---
+++ zhihu/main.py
@@ -0,0 +1,35 @@
+#encoding:utf-8
+from zhihu import Answer
+
+
+def vote_up_with_id():
+    data = Answer(id=14005147).vote_up()
+    print(data)
+
+
+from zhihu.models import Model
+from zhihu.models.account import Account
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == '__main__':
+    # vote_up_with_id()
+
+    # model = Account()
+    # model.login("ssss@qq.com", "ssss")
+    # model.login("158", "xxxx")
+
+    from zhihu import Zhihu
+
+    # zhihu = Zhihu()
+    # print(zhihu)
+    # profile  = zhihu.user(user_slug="xiaoxiaodouzi")
+    # print(profile)
+
+    from zhihu import Account
+
+    account = Account()
+    account.register("续航小三", phone_num="+33752962193", password="zhihudetail")
+
