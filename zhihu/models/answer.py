b7fb358911d3dcbc2a8d541f482f8c4bbd56b48c
wabc1994 <liuxiongcheng145@gmail.com>
2017-05-18 16:48:41 +0800

    answer累，包括赞同，感谢，中立，up down neutral

---
+++ zhihu/models/answer.py
@@ -0,0 +1,66 @@
+#encoding: utf-8
+
+import re
+
+from zhihu.auth import need_login
+from zhihu.error import ZhihuError
+from zhihu.models import Model
+from zhihu.url import URL
+
+class Answer(Model):
+    def __init__(self,id=None,url=None):
+        id=id if id is not None  else self.__extract_id(url)
+        if not id:
+            raise ZhihuError("没有制定回答的id或者url")
+        self.id=str(id)
+        super(Answer,self).__init__()
+
+
+    @staticmethod
+    def _extract_id(url):
+        pattern=re.compile("http://www.zhihu.com/question/\d+/answer/([\w-]+)")
+        match=pattern.search(url)
+        return match.group(1) if match else None
+    @need_login
+    def vote_up(self,**kwargs):
+        r=self._execute(url=URL.vote_up(self.id),data={"type":"up"},**kwargs)
+        if r.ok:
+            return r.json()
+        else:
+            raise ZhihuError("操作失败： %s" %r.text)
+    @need_login
+    def vote_down(self,**kwargs):
+        r=self._execute(url=URL.vote_down(self.id),data={"type":"down"},**kwargs)
+        if r.ok:
+            return r.json
+        else:
+            raise ZhihuError("操作失败： %s" %r.text)
+    @need_login
+    def vote_neutral(self,**kwargs):
+        r = self._execute(url=URL.vote_neutral(self.id), data={"type": "neutral"}, **kwargs)
+        if r.ok:
+            return r.json()
+        else:
+            raise ZhihuError("操作失败：%s" % r.text)
+
+    @need_login
+    def thank(self, **kwargs):
+        """
+        感谢
+        """
+        r = self._execute(url=URL.thank(self.id), **kwargs)
+        if r.ok:
+            return r.json()
+        else:
+            raise ZhihuError("操作失败：%s" % r.text)
+
+    @need_login
+    def thank_cancel(self, **kwargs):
+        """
+        感谢取消
+        """
+        r = self._execute(method="delete", url=URL.thank_cancel(self.id), **kwargs)
+        if r.ok:
+            return r.json()
+        else:
+            raise ZhihuError("操作失败：%s" % r.text)
